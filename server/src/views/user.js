85fb2dcab9f682f221a04cd441718e79404988b1
Rudy <rudyalvs@gmail.com>
2024-12-22 21:22:57 +0100

    rename view user => v_users

--- server/src/views/user.js
+++ server/src/views/user.js
@@ -1,7 +1,7 @@
 const mariadb = require("../database/db");
 
 users = mariadb.query(
-  "CREATE VIEW IF NOT EXISTS Utilisateur AS SELECT name, firstname, mail, login FROM users"
+  "CREATE VIEW IF NOT EXISTS v_users AS SELECT name, firstname, mail, login FROM users"
 );
 
 module.exports = users;
