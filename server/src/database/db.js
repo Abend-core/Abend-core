6698ce9afbe09ba0526e66dd89b3905be5bbde83
Rudy <rudyalvs@gmail.com>
2024-12-22 18:57:22 +0100

    rename bdd

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,23 +1,23 @@
-const {Sequelize, DataTypes} = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
 
 //Instanciation de la bdd
-const mariadb = new Sequelize('firstNode', 'root', '', {
-  host: 'localhost',
+const mariadb = new Sequelize("abend-core", "root", "", {
+  host: "localhost",
   port: 3306,
-  dialect: 'mariadb'
-})
+  dialect: "mariadb",
+});
 
-async function connect(){
+async function connect() {
   try {
     await mariadb.authenticate();
-    console.log('Vous êtes connecté à la base de donnée');
+    console.log("Vous êtes connecté à la base de donnée");
   } catch (error) {
-    console.error('Erreur de connection', error);
+    console.error("Erreur de connection", error);
   }
 }
 
 //Test si la connection c'est bien effectué.
-connect()
+connect();
 
 //Export de l'instance de bdd, afin de l'utilisé dans d'autre fichier.
-module.exports = mariadb
+module.exports = mariadb;
