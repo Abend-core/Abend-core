558b27041552ceaba2d3507792d38776ba28a30d
Rudy <rudyalvs@gmail.com>
2025-01-03 20:47:36 +0100

    role condition update

--- server/src/middleware/role.js
+++ server/src/middleware/role.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   const role = req.headers.role;
 
-  if (role == 0) {
+  if (role != 0) {
     return res.status(401).json({
       message:
         "L'utilisateur n'est pas administrateur, donc il n'est pas autorisé à accéder à cette ressource.",
