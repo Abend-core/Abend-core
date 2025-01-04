fd6c385cc6bc369d2f96538d882eb034111913cc
Lucas-ui <lucas7500@hotmail.fr>
2025-01-04 11:42:54 +0100

    Modifcation infos user et changement interface page profil

--- client/src/views/Profil.vue
+++ client/src/views/Profil.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="layout-profil h-screen flex">
+  <div class="layout-profil h-full flex">
     <aside class="sidebar w-[20%] p-[100px]">
       <div class="left-section-profil text-center mt-11">
-        <p class="mb-3">Mon profil</p>
-        <p class="mb-3">Gérer ses modules</p>
-        <p class="mb-3">Modifier son mot de passe</p>
+        <p class="mb-3 cursor-pointer" @click="setActiveSection('profile')">
+          Mon profil
+        </p>
+        <p class="mb-3 cursor-pointer" @click="setActiveSection('editInfo')">
+          Modifier mes informations
+        </p>
+        <p
+          class="mb-3 cursor-pointer"
+          @click="setActiveSection('manageModules')"
+        >
+          Gérer ses modules
+        </p>
       </div>
     </aside>
     <main class="content bg-[#FDFDFD] w-[80%] mr-[100px] relative">
@@ -25,89 +34,163 @@
               <p class="text-[24px]">{{ user.firstname }}</p>
               <p class="text-[24px]">{{ user.name }}</p>
             </div>
-            <div class="profil-save">
+            <div class="profil-save" v-if="activeSection === 'editInfo'">
               <button
                 class="absolute right-6 top-[250px] bg-[#3A84F5] p-[6px] rounded-md text-white"
+                @click="updateUserProfile"
               >
                 Modifier
               </button>
             </div>
           </div>
-          <div class="profil-data-user pl-[226px]">
-            <div class="flex flex-col gap-[22px]">
-              <div class="flex items-center">
-                <p class="w-[150px] mr-[200px]">Prénom</p>
-                <input class="w-[450px]" type="text" placeholder="Prénom" />
+          <div v-if="activeSection === 'profile'">
+            <div class="profil-data-user">
+              <div class="profil-data">
+                <p>{{ user.firstname }}</p>
+                <p>{{ user.name }}</p>
+                <p>{{ user.mail }}</p>
+                <p>{{ user.birth }}</p>
+                <p>{{ user.login }}</p>
               </div>
-              <div class="flex items-center">
-                <p class="w-[150px] mr-[200px]">Nom</p>
-                <input class="w-[450px]" type="text" placeholder="Nom" />
+            </div>
+          </div>
+          <div v-if="activeSection === 'editInfo'">
+            <div class="profil-edit-data pl-[226px]">
+              <div class="flex flex-col gap-[22px]">
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Prénom</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Prénom"
+                    v-model="prenomProfil"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Nom</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Nom"
+                    v-model="nomProfil"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Email</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Email"
+                    v-model="emailProfil"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Date de naissance</p>
+                  <input
+                    class="w-[450px]"
+                    type="date"
+                    placeholder="Date de naissance"
+                    v-model="birthProfil"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Identifiant</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Identifiant"
+                    v-model="identifiantProfil"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Ancien mot de passe</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Ancien mot de passe"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Nouveau mot de passe</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Nouveau mot de passe"
+                  />
+                </div>
+                <div class="flex items-center">
+                  <p class="w-[150px] mr-[200px]">Répéter le mot de passe</p>
+                  <input
+                    class="w-[450px]"
+                    type="text"
+                    placeholder="Répéter le mot de passe"
+                  />
+                </div>
               </div>
-              <div class="flex items-center">
-                <p class="w-[150px] mr-[200px]">Email</p>
-                <input class="w-[450px]" type="text" placeholder="Email" />
-              </div>
-              <div class="flex items-center">
-                <p class="w-[150px] mr-[200px]">Date de naissance</p>
-                <input
-                  class="w-[450px]"
-                  type="text"
-                  placeholder="Date de naissance"
-                />
-              </div>
-              <div class="flex items-center">
-                <p class="w-[150px] mr-[200px]">Identifiant</p>
-                <input
-                  class="w-[450px]"
-                  type="text"
-                  placeholder="Identifiant"
-                />
-              </div>
+            </div>
+          </div>
+          <div v-if="activeSection === 'manageModules'">
+            <div class="profil-manage-modules">
+              <p class="text-center">Gérer ses modules</p>
             </div>
           </div>
         </div>
       </div>
     </main>
   </div>
-  <!-- <div class="profil-edit-data">
-    <div class="profil-data">
-      <p>{{ user.firstname }}</p>
-      <p>{{ user.name }}</p>
-      <p>{{ user.mail }}</p>
-      <p>{{ user.birth }}</p>
-      <p>{{ user.login }}</p>
-      <button>Modifier</button>
-      <button>Sauvegarder</button>
-    </div>
-  </div>
-  <div class="profil-edit-password">
-    <input type="text" placeholder="Ancien mot de passe" />
-    <input type="text" placeholder="Nouveau mot de passe" />
-    <input type="text" placeholder="Répéter le mot de passe" />
-    <button>Modifier</button>
-  </div> -->
-  <!-- <div class="profil-manage-modules">
-    <p>Gestion des modules</p>
-  </div> -->
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { getUserById } from "../api/user";
+import { getUserById, editUserById } from "../api/user";
 
 defineEmits(["login", "logout"]);
 
-const user = ref([]);
 const id = sessionStorage.getItem("id");
+const user = ref([]);
+const activeSection = ref("profile");
+const prenomProfil = ref("");
+const nomProfil = ref("");
+const emailProfil = ref("");
+const birthProfil = ref("");
+const identifiantProfil = ref("");
+
+const updateUserProfile = async () => {
+  const updatedData = {
+    firstname: prenomProfil.value,
+    name: nomProfil.value,
+    mail: emailProfil.value,
+    birth: birthProfil.value,
+    login: identifiantProfil.value,
+  };
+
+  try {
+    await editUserById(id, updatedData);
+    getInfosProfil();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const setActiveSection = (section) => {
+  activeSection.value = section;
+};
 
 const getInfosProfil = async () => {
   try {
     const response = await getUserById(id);
     user.value = response.data.user;
+
+    prenomProfil.value = user.value.firstname;
+    nomProfil.value = user.value.name;
+    emailProfil.value = user.value.mail;
+    birthProfil.value = user.value.birth;
+    identifiantProfil.value = user.value.login;
   } catch (error) {
     console.error(error);
   }
 };
+
 getInfosProfil();
 </script>
 
