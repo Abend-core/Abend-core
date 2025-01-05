552427266bbdfc473e662e3198884b0cccf19916
Lucas-ui <lucas7500@hotmail.fr>
2025-01-05 10:11:32 +0100

    Correction database module

--- server/src/database/data/module.js
+++ server/src/database/data/module.js
@@ -21,7 +21,7 @@ const modules = [
     isShow: 1,
   },
   {
-    id: "",
+    id: "01936500-eb36-8471-999e-f1f94691712e",
     name: "Portfolio Lucas",
     link: "https://lucas-rolland.netlify.app/",
     color: "##6DB1E4",
