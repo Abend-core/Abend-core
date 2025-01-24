157e6f3a1e56431a0d2373e2e6e7fe109854e7e7
Rudy <rudyalvs@gmail.com>
2025-01-24 16:45:27 +0100

    fix init.ts

--- server/src/database/init.ts
+++ server/src/database/init.ts
@@ -27,119 +27,66 @@ if(env == 'Dev'){
     .catch((err) => {
       console.log("Erreur de synchronisation :", err);
     });
-}else{
-  sequelize
-    .sync({ force: true })
-    .then(async (_) => {
-      
-      pushDb_dev()
-    
-    })
-    .catch((err) => {
-      console.log("Erreur de synchronisation :", err);
-    });
 }
 
 async function pushDb_dev(){
   try {
-    for (const data of dataStatut.statuts) {
-      await Statut.create(data);
-      }
+      console.log("Début de synchronisation...")
+      initStatus()
       console.log("");
-      console.log("Statut insérés avec succès.");
-      for (const data of dataUser.users) {
-        data.id = "";
-        if (data.image == undefined) {
-          data.image = "bank-img-" + Math.trunc(Math.random() * 29) + ".png";
-        }
-
-        while (data.id === "") {
-          const uuid = NewUUID();
-          const user = await User.findByPk(uuid);
-          if (!user) {
-            data.id = uuid;
-            lastUUID = uuid;
-          }
-        }
-        await User.create(data);
-        data.password = await hash(data.password);
-        await User.update(data, {
-          where: { id: lastUUID },
-          validate: false,
-        });
-      }
+      initUsers()
       console.log("");
-      console.log("Utilisateurs insérés avec succès.");
+      initModules();
       console.log("");
-      for (const data of dataModule.modules) {
-        data.id = "";
-
-        while (data.id === "") {
-          const uuid = NewUUID();
-          const test = await Module.findByPk(uuid);
-          if (!test) {
-            data.id = uuid;
-          }
-        }
-      data.user_id = lastUUID;
-      await Module.create(data);
-    }
-    console.log("Modules insérés avec succès.");
-    console.log("");
-    console.log("Synchronisation effectuée !");
+      console.log("Synchronisation terminée !");
   } catch (err) {
     console.error("Erreur :", err);
   }
 }
 
-async function pushDb_prod(){
-  try {
-    for (const data of dataStatut.statuts) {
-      await Statut.create(data);
-      }
-      console.log("");
-      console.log("Statut insérés avec succès.");
-      for (const data of dataUser.users) {
-        data.id = "";
-        if (data.image == undefined) {
-          data.image = "bank-img-" + Math.trunc(Math.random() * 29) + ".png";
-        }
-
-        while (data.id === "") {
-          const uuid = NewUUID();
-          const user = await User.findByPk(uuid);
-          if (!user) {
-            data.id = uuid;
-            lastUUID = uuid;
-          }
-        }
-        await User.create(data);
-        data.password = await hash(data.password);
-        await User.update(data, {
-          where: { id: lastUUID },
-          validate: false,
-        });
-      }
-      console.log("");
-      console.log("Utilisateurs insérés avec succès.");
-      console.log("");
-      for (const data of dataModule.modules) {
-        data.id = "";
-
-        while (data.id === "") {
-          const uuid = NewUUID();
-          const test = await Module.findByPk(uuid);
-          if (!test) {
-            data.id = uuid;
-          }
-        }
-      data.user_id = lastUUID;
-      await Module.create(data);
+async function initStatus(){
+  for (const data of dataStatut.statuts) {
+    await Statut.create(data);
     }
-    console.log("Modules insérés avec succès.");
-    console.log("");
-    console.log("Synchronisation effectuée !");
-  } catch (err) {
-    console.error("Erreur :", err);
+  console.log("Status insérés avec succès.");
+}
+async function initUsers(){
+  for (const data of dataUser.users) {
+    data.id = "";
+    if (data.image == undefined) {
+      data.image = "bank-img-" + Math.trunc(Math.random() * 29) + ".png";
+    }
+
+    while (data.id === "") {
+      const uuid = NewUUID();
+      const user = await User.findByPk(uuid);
+      if (!user) {
+        data.id = uuid;
+        lastUUID = uuid;
+      }
+    }
+    await User.create(data);
+    data.password = await hash(data.password);
+    await User.update(data, {
+      where: { id: lastUUID },
+      validate: false,
+    });
   }
+  console.log("Utilisateurs insérés avec succès.");
+} 
+async function initModules(){
+  for (const data of dataModule.modules) {
+    data.id = "";
+
+    while (data.id === "") {
+      const uuid = NewUUID();
+      const test = await Module.findByPk(uuid);
+      if (!test) {
+        data.id = uuid;
+      }
+    }
+  data.user_id = lastUUID;
+  await Module.create(data);
+  }
+  console.log("Modules insérés avec succès.");
 }
