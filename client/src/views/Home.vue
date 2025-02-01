a175d016d8e65a44624283dfc2ef9fbfc4aa4a0a
Lucas-ui <lucas7500@hotmail.fr>
2025-02-01 22:20:35 +0100

    Je sais pas trop

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,104 +1,81 @@
 <template>
-  <main class="p-2 max-w-[1400px] mx-auto">
-    <div class="flex items-center justify-center flex-wrap gap-10 mt-16 mb-16">
-      <div class="flex" v-for="module in modules" :key="module.id">
-        <a
-          :href="module.link"
-          class="module-card w-[300px] lg:w-[400px] h-[150px] lg:h-[200px] rounded-2xl relative bg-[#141A22] text-white"
-          :style="{
-            border: `1px solid black`,
-          }"
-          target="_blank"
-        >
-          <img
-            class="absolute w-[40px] h-[40px] lg:w-[50px] lg:h-[50px] right-3 top-3 rounded-full border-[2px] border-white p-[2px] box-border"
-            :src="`${apiUrl}/uploadsFile/module/${module.image}`"
-            alt="Photo du module"
-            loading="lazy"
-          />
-          <div class="p-3 h-full">
-            <div class="flex items-center gap-2">
-              <p class="text-base lg:text-xl font-bold">{{ module.name }}</p>
-              <i
-                class="ri-verified-badge-fill text-xl lg:text-2xl text-white cursor-pointer"
-              ></i>
-            </div>
-            <div>
-              <p class="mt-4 lg:mt-6 text-sm lg:text-base">
-                {{ module.content }}
-              </p>
-            </div>
-            <p class="absolute bottom-2 lg:bottom-4 text-[10px] lg:text-xs">
-              {{ module.User.username }}
-            </p>
-            <i
-              v-if="getEtatLike(module.id)"
-              class="ri-heart-fill absolute bottom-2 lg:bottom-3 right-3 lg:right-4 text-xl lg:text-2xl cursor-pointer text-red-500 z-10"
-              @click="toggleLike(module.id, $event)"
-            ></i>
-            <i
-              v-else
-              class="ri-heart-line absolute bottom-2 lg:bottom-3 right-3 lg:right-4 text-xl lg:text-2xl cursor-pointer z-10"
-              @click="toggleLike(module.id, $event)"
-            ></i>
+  <div>
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+      <div
+        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-4xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+        ></div>
+      </div>
+
+      <div class="mx-auto z-30 max-w-2xl py-32 sm:py-48 lg:py-48">
+        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div
+            class="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-500 dark:ring-1 dark:ring-gray-700 hover:dark:ring-gray-200/20"
+          >
+            Gérez facilement tous vos liens favoris dans un seul endroit.
+            <a href="#" class="font-semibold text-primaryRed"
+              ><span class="absolute inset-0" aria-hidden="true"></span>En
+              savoir plus <span aria-hidden="true">&rarr;</span></a
+            >
           </div>
-        </a>
+        </div>
+        <div class="text-center">
+          <h1
+            class="text-5xl font-semibold tracking-tight text-balance text-gray-800 dark:text-gray-200 sm:text-7xl"
+          >
+            Créez votre propre <span class="text-primaryRed">hub</span> de liens
+            <span class="text-primaryRed">favoris</span>
+          </h1>
+          <p
+            class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"
+          >
+            Organisez et accédez rapidement à vos pages favorites grâce à des
+            modules de redirection personnalisés.
+          </p>
+
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              class="rounded-md bg-primaryRed px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs"
+              >Créer mon hub</a
+            >
+            <a href="#" class="text-sm/6 font-semibold text-gray-900"
+              >En savoir plus <span aria-hidden="true">→</span></a
+            >
+          </div>
+        </div>
+      </div>
+      <div
+        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
       </div>
     </div>
-  </main>
+  </div>
 </template>
-
-<script setup>
-import { ref } from "vue";
-import { findAllModulesVisible } from "../api/module";
-import { formatDate } from "../utils/date";
-
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
-const modules = ref([]);
-const etatLike = ref({});
-
-const allModules = async () => {
-  try {
-    const response = await findAllModulesVisible();
-    modules.value = response.data.module;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const getEtatLike = (idModule) => {
-  return etatLike.value[idModule];
-};
-
-const toggleLike = (idModule, event) => {
-  etatLike.value[idModule] = !etatLike.value[idModule];
-  event.stopPropagation();
-  event.preventDefault();
-};
-
-allModules();
-</script>
-
-<style scoped>
-input {
-  border: 1px solid #d1d9e0;
-  display: block;
-  width: 100%;
-}
-
-input,
-button {
-  padding: 5px 12px;
-  font-size: 14px;
-  border-radius: 6px;
-}
-
-.module-card {
-  transition: transform 0.3s ease;
-}
-
-.module-card:hover {
-  transform: translateY(-10px) scale(1.05);
-}
-</style>
