e35715b34f5e3c345760e589b96975975f6d8ced
Lucas-ui <lucas7500@hotmail.fr>
2025-02-02 16:50:12 +0100

    Modification test push

--- client/src/layouts/Navbar.vue
+++ client/src/layouts/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700"
+    class="w-full dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 sticky top-0 backdrop-blur-lg"
     @keydown.esc="closeModal('searchBar')"
     tabindex="0"
   >
