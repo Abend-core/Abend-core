7fbae863887ffb87d40b6fc51039e7f70690033d
Lucas-ui <lucas7500@hotmail.fr>
2025-02-07 13:53:41 +0100

    Fix: css editinfos

--- client/src/components/profil/editInfos.vue
+++ client/src/components/profil/editInfos.vue
@@ -121,7 +121,7 @@
       </form>
       <div class="flex justify-end">
         <button
-          class="p-paddingSm rounded-md text-primaryRed border border-primaryRed w-fit mb-4"
+          class="p-paddingSm rounded-md text-primaryRed border border-primaryRed w-fit mb-4 hover:bg-primaryRed hover:text-gray-800"
           @click="toggleModalConfirmDeleteUser"
         >
           Supprimer mon compte
